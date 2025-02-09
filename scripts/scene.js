42be6c7af7c35b2d9560d061230804440b2a7cd4
Yeoweiquan <yeoweiquan@gmail.com>
2025-02-09 15:35:56 +0800

    Update scene.js

--- scripts/scene.js
+++ scripts/scene.js
@@ -21,7 +21,7 @@ export var createScene = async function (engine, canvas) {
     // Our built-in 'ground' shape.
     var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
 
-    BABYLON.SceneLoader.ImportMeshAsync("tunnel", "../resource/models/", "tunnel.glb", scene).then((result) => {
+    BABYLON.SceneLoader.ImportMeshAsync("", "../resource/models/", "tunnel.glb", scene).then((result) => {
         result.meshes[1].position.x = 20;
         const myMesh1 = scene.getMeshByName("Tunnel");
         myMesh1.rotation.y = Math.PI / 2;
